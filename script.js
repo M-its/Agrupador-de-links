496ad2732a8a2527d482f9490370262701b71c4c
Mitsrael Souza <mitarael9@gmail.com>
2023-06-24 15:34:17 -0300

    add some customizations and a gradient border

--- script.js
+++ script.js
@@ -4,15 +4,4 @@ function toggleMode() {
 
   // pegar a tag image
   const img = document.querySelector("#profile img")
-
-  // substituir a imagem
-  if (html.classList.contains("light")) {
-    // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute("alt", "imagem de avatar do usuário Mayk Brito modo light")
-  } else {
-    // se tiver sem light mode, manter a imagem original
-    img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute("alt", "imagem de avatar do usuário Mayk Brito modo dark")
-  }
 }
